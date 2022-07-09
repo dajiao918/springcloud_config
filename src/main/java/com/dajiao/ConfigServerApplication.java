22c0da9efb5d8f322116c0dd334c0d63a854cc23
Yu <yu2018010029@outlook.com>
2022-07-09 09:27:10 +0800

    cloud center config

---
+++ src/main/java/com/dajiao/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.dajiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: Mr.Yu
+ * @create: 2022-07-09 09:20
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
